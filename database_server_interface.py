481bfb42f45d1152fac9300315bf10abc41cdf14
Pasindu Rathnayaka <86573066+PasinduNimesha@users.noreply.github.com>
2026-05-02 17:33:20 +0530

    Add database server interface module
    A lightweight Database class for SQLite (sqlite3 + pandas) with run_query and run_query_params, and utility functions database_connect (SQL Server via SQLAlchemy+pyodbc) and database_run_query returning DataFrame. Includes a simple example main usage to query a local sqlite database. Intended to centralize DB access for local sqlite files and remote MSSQL servers.

---
+++ database_server_interface.py
@@ -0,0 +1,64 @@
+from sqlalchemy.engine import URL
+import pandas as pd
+import sqlalchemy as sa
+import sqlite3
+
+
+class Database:
+    def __init__(self, database_path: str):
+        # Connect the db in the current working directory,
+        # implicitly creating one if it does not exist.
+        self._con = sqlite3.connect(database_path)
+
+        # Creating a curser.
+        self._cur = self._con.cursor()
+
+    def run_query(self, query: str):
+        self._cur.execute(query)
+        return pd.read_sql_query(query, self._con)
+
+    def run_query_params(self, query: str, params: tuple):
+        return pd.read_sql_query(query, self._con, params=params)
+
+    def __del__(self):
+        try:
+            # Committing changes
+            self._con.commit()
+
+            # Closing the connection
+            self._con.close()
+        except:
+            pass
+
+
+def database_connect(server_name: str, database_name: str, username='', password='') -> sa.engine.Engine:
+    """Establish a connection with SQL database server.
+
+    :param server_name:   Input server name.
+    :param database_name: Input database name.
+    :param username:      Input username.
+    :param password:      Input password.
+    :return:              The database engine.
+    """
+    connection_string = f'DRIVER={{SQL Server}};SERVER={server_name};DATABASE={database_name};' \
+                        f'UID={username};PWD={password}'
+    connection_url = URL.create("mssql+pyodbc", query={"odbc_connect": connection_string})
+    engine = sa.create_engine(connection_url)
+    return engine
+
+
+def database_run_query(input_db_engine: sa.engine.Engine, query: str) -> pd.DataFrame:
+    """Run a query on the database.
+
+    :param input_db_engine: The input database engine.
+    :param query:     An input SQL query.
+    :return:          The query dataframe result.
+    """
+    with input_db_engine.begin() as conn:
+        result_df = pd.read_sql_query(sa.text(query), conn)
+    return result_df
+
+
+if __name__ == '__main__':
+    db = Database("databases/sqlite_databases/the_movies_database.db")
+    print(db.run_query("SELECT * FROM candidates WHERE candidate_id=3"))
